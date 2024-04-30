8861fbb190d6b480aafc6c59a6f30f4140484962
Tom Saintenoy <tom.saintenoy@ecoles-epsi.net>
2024-04-30 12:26:01 +0200

    Change mdp

--- Client/client-ftp.py
+++ Client/client-ftp.py
@@ -1,30 +1,35 @@
 from ftplib import FTP
 
-# Adresse IP du serveur FTP (dans notre cas, c'est localhost)
+# Adresse IP du serveur FTP
 host = "127.0.0.1"
 # Nom d'utilisateur et mot de passe pour se connecter au serveur FTP
 user = "epsi"
-password = "client24"
+password = "client25"
 
 # Fonction pour télécharger un fichier depuis le serveur FTP
 def download_file(filename):
-    # Établir une connexion FTP
     with FTP(host) as ftp:
         # Se connecter au serveur avec le nom d'utilisateur et le mot de passe
         ftp.login(user=user, passwd=password)
+        print(ftp.getwelcome())
         # Télécharger le fichier depuis le serveur
         with open(filename, 'wb') as file:
             ftp.retrbinary('RETR ' + filename, file.write)
 
 # Fonction pour téléverser un fichier vers le serveur FTP
 def upload_file(filename):
-    # Établir une connexion FTP
     with FTP(host) as ftp:
         # Se connecter au serveur avec le nom d'utilisateur et le mot de passe
         ftp.login(user=user, passwd=password)
+        print(ftp.getwelcome())
         # Téléverser le fichier vers le serveur
         with open(filename, 'rb') as file:
             ftp.storbinary('STOR ' + filename, file)
+
+def change_password(new_password):
+    with FTP(host) as ftp:
+        ftp.login(user=user, passwd=password)
+        ftp.sendcmd('CMD_CHPASS ' + new_password)
 
 # Exemple d'utilisation :
 # Télécharger un fichier depuis le serveur FTP
@@ -32,3 +37,5 @@ download_file("exemple.txt")
 
 # Téléverser un fichier vers le serveur FTP
 #upload_file("exemple.txt")
+
+#change_password("client25")
--- Server/server-ftp.py
+++ Server/server-ftp.py
@@ -1,17 +1,38 @@
+import os
 from pyftpdlib.authorizers import DummyAuthorizer
 from pyftpdlib.handlers import FTPHandler
 from pyftpdlib.servers import FTPServer
 
+class MyHandler(FTPHandler):
+    def ftp_CMD_CHPASS(self, line):
+        new_password = line.strip()
+        new_password = os.path.basename(new_password)
+        user = self.username
+        self.authorizer.remove_user(user)
+        # Mettez à jour le mot de passe de l'utilisateur
+        self.authorizer.add_user(user, new_password, '.', perm="elradfmwMT")
+        self.respond("230 Mot de passe modifié avec succès.")
+        self.handle_close()
+
 # Créer une instance d'autorisateur fictif avec l'utilisateur et le mot de passe spécifiés
 authorizer = DummyAuthorizer()
-authorizer.add_user("epsi", "client24", ".", perm="elradfmw")
+authorizer.add_user("epsi", "client24", ".", perm="elradfmwMT")
 
 # Permettre l'accès anonyme avec des autorisations limitées
-authorizer.add_anonymous(".", perm="elradfm")
+authorizer.add_anonymous(os.getcwd())
 
 # Créer une instance de gestionnaire FTP et spécifier l'autorisateur
-handler = FTPHandler
+handler = MyHandler
 handler.authorizer = authorizer
+
+handler.banner = "bienvenue sur le serveur de l'epsi"
+handler.proto_cmds['CMD_CHPASS'] = {
+    'arg': True,
+    'perm': 'a',
+    'auth': True,
+    'callback': MyHandler.ftp_CMD_CHPASS,
+    'help': 'Change user password'
+}
 
 # Créer une instance de serveur FTP écoutant sur l'adresse locale 127.0.0.1 et le port 21
 server = FTPServer(("127.0.0.1", 21), handler)
