b37c10fe2e22fd5e17c5073cfb8de9dda31670d2
LoanH <lheniart@be-infotech.fr>
2024-04-30 13:56:01 +0200

    client and file

--- Client/client-ftp.py
+++ Client/client-ftp.py
@@ -1,7 +1,39 @@
 import tkinter as tk
-from tkinter import messagebox, filedialog
+from tkinter import ttk, messagebox, filedialog
 from ftplib import FTP
 import os
+
+ftp_server = "localhost"
+
+
+def login():
+    username = username_entry.get()
+    password = password_entry.get()
+    if username and password:
+        try:
+            with FTP(ftp_server) as ftp:
+                ftp.login(user=username, passwd=password)
+                messagebox.showinfo("Connexion réussie", "Vous êtes connecté au serveur FTP.")
+                refresh_files_list()
+                change_password_frame.pack(side=tk.LEFT, padx=10, pady=10, fill=tk.BOTH, expand=True)
+                upload_frame.pack(side=tk.LEFT, padx=10, pady=10, fill=tk.BOTH, expand=True)
+                server_file_list_frame.pack(side=tk.RIGHT, padx=10, pady=10, fill=tk.BOTH, expand=True)
+        except Exception as e:
+            messagebox.showerror("Erreur de connexion", f"Impossible de se connecter au serveur FTP : {str(e)}")
+    else:
+        messagebox.showerror("Erreur", "Veuillez saisir un nom d'utilisateur et un mot de passe.")
+
+
+def change_password():
+    new_password = new_password_entry.get()
+    if new_password:
+        with FTP(ftp_server) as ftp:
+            ftp.login(user=username_entry.get(), passwd=password_entry.get())
+            response = ftp.sendcmd('CMD_CHPASS ' + new_password)
+            messagebox.showinfo("Changement de mot de passe", "Le mot de passe a été changé avec succès.")
+    else:
+        messagebox.showerror("Erreur", "Veuillez saisir un nouveau mot de passe.")
+
 
 def upload_file():
     file_path = filedialog.askopenfilename()
@@ -17,6 +49,7 @@ def upload_file():
     else:
         messagebox.showerror("Erreur", "Aucun fichier sélectionné.")
 
+
 def download_file():
     filename = selected_file.get()
     if filename:
@@ -27,10 +60,14 @@ def download_file():
     else:
         messagebox.showerror("Erreur", "Veuillez sélectionner un fichier à télécharger.")
 
+
 def refresh_files_list():
     # Actualiser la liste des fichiers sur le serveur
     server_file_list.delete(0, tk.END)
-    ftp.retrlines("LIST", lambda line: server_file_list.insert(tk.END, extract_file_info(line)))
+    with FTP(ftp_server) as ftp:
+        ftp.login(user=username_entry.get(), passwd=password_entry.get())
+        ftp.retrlines("LIST", lambda line: server_file_list.insert(tk.END, extract_file_info(line)))
+
 
 def extract_file_info(line):
     parts = line.split(maxsplit=8)
@@ -38,71 +75,72 @@ def extract_file_info(line):
     modification_date = ' '.join(parts[-4:-1])
     return f"{file_name} ({modification_date})"
 
-def select_file(event):
-    selected_item = server_file_list.curselection()
-    if selected_item:
-        selected_filename = server_file_list.get(selected_item)
-        selected_filename = selected_filename.split()[0]  # Récupérer le nom du fichier sans la date
-        selected_file.set(selected_filename)
-
-ftp_server = "localhost"
-username = "epsi"
-password = "client24"
-
-ftp = FTP()
-ftp.connect(ftp_server)
-ftp.login(username, password)
 
 root = tk.Tk()
 root.title("Gestionnaire FTP")
 root.geometry("800x400")  # Définir la taille de la fenêtre principale
 
-upload_frame = tk.Frame(root)
-upload_frame.pack(side=tk.LEFT, padx=10, pady=10, fill=tk.BOTH, expand=True)
+# Header (connexion / changement de mot de passe)
+header_frame = ttk.Frame(root)
+header_frame.pack(side=tk.TOP, fill=tk.BOTH, expand=True)
 
-upload_label = tk.Label(upload_frame, text="Fichiers à uploader:")
+username_label = ttk.Label(header_frame, text="Nom d'utilisateur:")
+username_label.grid(row=0, column=0, padx=5, pady=5, sticky=tk.E)
+
+username_entry = ttk.Entry(header_frame)
+username_entry.grid(row=0, column=1, padx=5, pady=5)
+
+password_label = ttk.Label(header_frame, text="Mot de passe:")
+password_label.grid(row=1, column=0, padx=5, pady=5, sticky=tk.E)
+
+password_entry = ttk.Entry(header_frame, show="*")
+password_entry.grid(row=1, column=1, padx=5, pady=5)
+
+login_button = ttk.Button(header_frame, text="Se connecter", command=login)
+login_button.grid(row=2, columnspan=2, pady=5)
+
+change_password_frame = ttk.Frame(root)
+change_password_frame.pack(side=tk.LEFT, fill=tk.BOTH, expand=True)
+
+new_password_label = ttk.Label(change_password_frame, text="Nouveau mot de passe:")
+new_password_label.grid(row=0, column=0, padx=5, pady=5, sticky=tk.E)
+
+new_password_entry = ttk.Entry(change_password_frame)
+new_password_entry.grid(row=0, column=1, padx=5, pady=5)
+
+change_password_button = ttk.Button(change_password_frame, text="Changer le mot de passe", command=change_password)
+change_password_button.grid(row=1, columnspan=2, pady=5)
+
+# Centre (Upload)
+upload_frame = ttk.Frame(root)
+upload_frame.pack(side=tk.LEFT, fill=tk.BOTH, expand=True)
+
+upload_label = ttk.Label(upload_frame, text="Fichiers à uploader:")
 upload_label.pack(padx=5, pady=5, anchor=tk.W)
 
-upload_button = tk.Button(upload_frame, text="Uploader un fichier", command=upload_file)
+upload_button = ttk.Button(upload_frame, text="Uploader un fichier", command=upload_file)
 upload_button.pack(padx=5, pady=5)
 
-server_file_list_frame = tk.Frame(root)
-server_file_list_frame.pack(side=tk.RIGHT, padx=10, pady=10, fill=tk.BOTH, expand=True)
+# Droite (Download)
+server_file_list_frame = ttk.Frame(root)
+server_file_list_frame.pack(side=tk.RIGHT, fill=tk.BOTH, expand=True)
 
-server_file_list_label = tk.Label(server_file_list_frame, text="Fichiers sur le serveur:")
-server_file_list_label.pack(padx=5, pady=5, anchor=tk.W)
+refresh_files_button = ttk.Button(server_file_list_frame, text="Actualiser la liste des fichiers sur le serveur",
+                                  command=refresh_files_list)
+refresh_files_button.pack(side=tk.TOP, padx=10, pady=5)
 
 server_file_list = tk.Listbox(server_file_list_frame, width=50)
-server_file_list.pack(side=tk.LEFT, padx=5, pady=5, fill=tk.BOTH, expand=True)
+server_file_list.pack(side=tk.TOP, padx=5, pady=5, fill=tk.BOTH, expand=True)
 
-server_scrollbar = tk.Scrollbar(server_file_list_frame, orient="vertical", command=server_file_list.yview)
+server_scrollbar = ttk.Scrollbar(server_file_list_frame, orient="vertical", command=server_file_list.yview)
 server_scrollbar.pack(side=tk.RIGHT, fill="y")
 server_file_list.configure(yscrollcommand=server_scrollbar.set)
 
-server_file_list.bind("<<ListboxSelect>>", select_file)
+server_file_list.bind("<<ListboxSelect>>", lambda event: select_file())
 
-download_frame = tk.Frame(root)
-download_frame.pack(side=tk.BOTTOM, padx=10, pady=10, fill=tk.BOTH, expand=True)
+download_button = ttk.Button(server_file_list_frame, text="Télécharger le fichier sélectionné", command=download_file)
+download_button.pack(side=tk.BOTTOM, padx=10, pady=5)
 
 selected_file = tk.StringVar()
-selected_file_label = tk.Label(download_frame, text="Fichier sélectionné:")
-selected_file_label.grid(row=0, column=0, padx=5)
 
-selected_file_entry = tk.Entry(download_frame, textvariable=selected_file)
-selected_file_entry.grid(row=0, column=1, padx=5)
-
-download_button = tk.Button(download_frame, text="Télécharger le fichier sélectionné", command=download_file)
-download_button.grid(row=0, column=2, padx=5)
-
-refresh_files_button = tk.Button(root, text="Actualiser la liste des fichiers sur le serveur", command=refresh_files_list)
-refresh_files_button.pack(side=tk.TOP, padx=10, pady=5)
-
-welcome_text = ftp.getwelcome()
-welcome_label = tk.Label(root, text=welcome_text, font=("Helvetica", 12), wraplength=700)
-welcome_label.pack(side=tk.TOP, padx=10, pady=10, fill=tk.BOTH, expand=True)
-
-
-refresh_files_list()
 root.mainloop()
-
-ftp.quit()
