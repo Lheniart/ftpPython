a1b70130489c555ebc6ee571a8f608d9ec34d71f
LoanH <lheniart@be-infotech.fr>
2024-04-30 11:56:37 +0200

    client and file

--- Client/client-ftp.py
+++ Client/client-ftp.py
@@ -1,6 +1,21 @@
 import tkinter as tk
-from tkinter import messagebox
+from tkinter import messagebox, filedialog
 from ftplib import FTP
+import os
+
+def upload_file():
+    file_path = filedialog.askopenfilename()
+    if file_path:
+        filename = os.path.basename(file_path)
+        try:
+            with open(file_path, "rb") as file:
+                ftp.storbinary(f"STOR {filename}", file)
+            refresh_files_list()
+            messagebox.showinfo("Upload réussi", f"Le fichier {filename} a été uploadé avec succès.")
+        except Exception as e:
+            messagebox.showerror("Erreur d'upload", f"Une erreur s'est produite lors de l'upload du fichier : {str(e)}")
+    else:
+        messagebox.showerror("Erreur", "Aucun fichier sélectionné.")
 
 def download_file():
     filename = selected_file.get()
@@ -13,8 +28,9 @@ def download_file():
         messagebox.showerror("Erreur", "Veuillez sélectionner un fichier à télécharger.")
 
 def refresh_files_list():
-    file_list.delete(0, tk.END)
-    ftp.retrlines("LIST", lambda line: file_list.insert(tk.END, extract_file_info(line)))
+    # Actualiser la liste des fichiers sur le serveur
+    server_file_list.delete(0, tk.END)
+    ftp.retrlines("LIST", lambda line: server_file_list.insert(tk.END, extract_file_info(line)))
 
 def extract_file_info(line):
     parts = line.split(maxsplit=8)
@@ -23,9 +39,9 @@ def extract_file_info(line):
     return f"{file_name} ({modification_date})"
 
 def select_file(event):
-    selected_item = file_list.curselection()
+    selected_item = server_file_list.curselection()
     if selected_item:
-        selected_filename = file_list.get(selected_item)
+        selected_filename = server_file_list.get(selected_item)
         selected_filename = selected_filename.split()[0]  # Récupérer le nom du fichier sans la date
         selected_file.set(selected_filename)
 
@@ -39,28 +55,34 @@ ftp.login(username, password)
 
 root = tk.Tk()
 root.title("Gestionnaire FTP")
-root.geometry("600x400")  # Définir la taille de la fenêtre principale
+root.geometry("800x400")  # Définir la taille de la fenêtre principale
 
-file_list_frame = tk.Frame(root)
-file_list_frame.pack(padx=10, pady=10, fill=tk.BOTH, expand=True)
+upload_frame = tk.Frame(root)
+upload_frame.pack(side=tk.LEFT, padx=10, pady=10, fill=tk.BOTH, expand=True)
 
-file_list_label = tk.Label(file_list_frame, text="Dossiers distants:")
-file_list_label.grid(row=0, column=0, sticky=tk.W)
+upload_label = tk.Label(upload_frame, text="Fichiers à uploader:")
+upload_label.pack(padx=5, pady=5, anchor=tk.W)
 
-file_list = tk.Listbox(file_list_frame, width=50)
-file_list.grid(row=1, column=0, padx=10, pady=5, sticky=tk.W+tk.E+tk.N+tk.S)
+upload_button = tk.Button(upload_frame, text="Uploader un fichier", command=upload_file)
+upload_button.pack(padx=5, pady=5)
 
-scrollbar = tk.Scrollbar(file_list_frame, orient="vertical", command=file_list.yview)
-scrollbar.grid(row=1, column=1, sticky="ns")
-file_list.configure(yscrollcommand=scrollbar.set)
+server_file_list_frame = tk.Frame(root)
+server_file_list_frame.pack(side=tk.RIGHT, padx=10, pady=10, fill=tk.BOTH, expand=True)
 
-file_list.bind("<<ListboxSelect>>", select_file)
+server_file_list_label = tk.Label(server_file_list_frame, text="Fichiers sur le serveur:")
+server_file_list_label.pack(padx=5, pady=5, anchor=tk.W)
 
-refresh_button = tk.Button(root, text="Actualiser la liste des fichiers", command=refresh_files_list)
-refresh_button.pack(padx=10, pady=5)
+server_file_list = tk.Listbox(server_file_list_frame, width=50)
+server_file_list.pack(side=tk.LEFT, padx=5, pady=5, fill=tk.BOTH, expand=True)
+
+server_scrollbar = tk.Scrollbar(server_file_list_frame, orient="vertical", command=server_file_list.yview)
+server_scrollbar.pack(side=tk.RIGHT, fill="y")
+server_file_list.configure(yscrollcommand=server_scrollbar.set)
+
+server_file_list.bind("<<ListboxSelect>>", select_file)
 
 download_frame = tk.Frame(root)
-download_frame.pack(padx=10, pady=10, fill=tk.BOTH, expand=True)
+download_frame.pack(side=tk.BOTTOM, padx=10, pady=10, fill=tk.BOTH, expand=True)
 
 selected_file = tk.StringVar()
 selected_file_label = tk.Label(download_frame, text="Fichier sélectionné:")
@@ -72,8 +94,15 @@ selected_file_entry.grid(row=0, column=1, padx=5)
 download_button = tk.Button(download_frame, text="Télécharger le fichier sélectionné", command=download_file)
 download_button.grid(row=0, column=2, padx=5)
 
-refresh_files_list()
+refresh_files_button = tk.Button(root, text="Actualiser la liste des fichiers sur le serveur", command=refresh_files_list)
+refresh_files_button.pack(side=tk.TOP, padx=10, pady=5)
 
+welcome_text = ftp.getwelcome()
+welcome_label = tk.Label(root, text=welcome_text, font=("Helvetica", 12), wraplength=700)
+welcome_label.pack(side=tk.TOP, padx=10, pady=10, fill=tk.BOTH, expand=True)
+
+
+refresh_files_list()
 root.mainloop()
 
 ftp.quit()
--- Server/server-ftp.py
+++ Server/server-ftp.py
@@ -2,6 +2,7 @@ from pyftpdlib.authorizers import DummyAuthorizer
 from pyftpdlib.handlers import FTPHandler
 from pyftpdlib.servers import FTPServer
 
+
 # Créer une instance d'autorisateur fictif avec l'utilisateur et le mot de passe spécifiés
 authorizer = DummyAuthorizer()
 authorizer.add_user("epsi", "client24", ".", perm="elradfmw")
@@ -13,8 +14,12 @@ authorizer.add_anonymous(".", perm="elradfm")
 handler = FTPHandler
 handler.authorizer = authorizer
 
+handler.banner = "Bienvenue sur le serveur de l'epsi"
+
 # Créer une instance de serveur FTP écoutant sur l'adresse locale 127.0.0.1 et le port 21
 server = FTPServer(("127.0.0.1", 21), handler)
+
+
 
 # Démarrer le serveur FTP
 server.serve_forever()
